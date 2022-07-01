a90b798db8c5c396623ed23dbb0fe84b1614c9ac
Laura Velásquez Cano <laura.velasquez.cano@gmail.com>
2022-07-01 16:02:26 -0500

    Creating search view and binding view. Adding dimens file

---
+++ app/src/main/java/com/lauravelasquezcano/melichallenge/app/ui/main/search/SearchFragment.kt
@@ -0,0 +1,28 @@
+package com.lauravelasquezcano.melichallenge.app.ui.main.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lauravelasquezcano.melichallenge.R
+import com.lauravelasquezcano.melichallenge.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
