853a8c0041e5f7c903a752d457d7477cf2c87520
Laura Velásquez Cano <laura.velasquez.cano@gmail.com>
2022-07-02 11:59:46 -0500

    Creating main activity view

--- app/src/main/java/com/lauravelasquezcano/melichallenge/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/lauravelasquezcano/melichallenge/app/ui/main/MainActivity.kt
@@ -2,11 +2,28 @@ package com.lauravelasquezcano.melichallenge.app.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import com.lauravelasquezcano.melichallenge.R
+import com.lauravelasquezcano.melichallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        appBarConfiguration = AppBarConfiguration.Builder().build()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
     }
 }
