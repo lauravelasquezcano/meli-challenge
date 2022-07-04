7cf4dfaa7c8b4ace161553ef8d5529859abd2f63
Laura Velásquez Cano <laura.velasquez.cano@gmail.com>
2022-07-03 21:00:22 -0500

    Showing items image using glide library

--- app/src/main/java/com/lauravelasquezcano/melichallenge/app/ui/main/results/ResultsAdapter.kt
+++ app/src/main/java/com/lauravelasquezcano/melichallenge/app/ui/main/results/ResultsAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.lauravelasquezcano.melichallenge.R
 import com.lauravelasquezcano.melichallenge.databinding.ResultItemBinding
 import com.lauravelasquezcano.melichallenge.domain.Item
 
@@ -42,6 +44,12 @@ class ResultsAdapter : RecyclerView.Adapter<ResultsAdapter.ResultViewHolder>() {
                 if (item.currency_id.isNullOrEmpty()) tvCurrency.visibility =
                     View.GONE else tvCurrency.text = item.currency_id
             }
+            Glide.with(context)
+                .load(item.thumbnail)
+                .override(100, 100)
+                .placeholder(R.drawable.ic_image_holder)
+                .error(R.drawable.ic_broken_image)
+                .into(binding.ivItemImage)
         }
     }
 }
